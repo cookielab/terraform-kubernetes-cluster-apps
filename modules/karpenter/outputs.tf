1274d07671c1dc5b46e79a9036ca59a48a72d500
Jakub Jindra <jakub.jindra@cookielab.io>
2025-01-24 12:11:57 +0100

    chore(karpeneter): put module.irsa.iam_role_name on output

---
+++ modules/karpenter/outputs.tf
@@ -0,0 +1,3 @@
+output "node_role_name" {
+  value = module.irsa.iam_role_name
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "namespace" {
   value = local.namespace
 }
+
+output "karpenter_node_role_name" {
+  value = module.karpenter[0].node_role_name
+}
