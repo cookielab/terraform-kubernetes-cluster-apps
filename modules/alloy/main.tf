bd896e3bc447251e2077b01aae732b584fd26d5f
Cookie Monster <robot@cookielab.io>
2025-04-04 03:12:19 +0000

    chore(deps): update helm release alloy to v0.12.6

--- modules/alloy/main.tf
+++ modules/alloy/main.tf
@@ -13,7 +13,7 @@ resource "helm_release" "node" {
   chart      = "alloy"
   name       = "node-agent"
   namespace  = var.namespace
-  version    = "0.12.5"
+  version    = "0.12.6"
   values = [
     yamlencode({
       alloy = {
@@ -101,7 +101,7 @@ resource "helm_release" "cluster" {
   chart      = "alloy"
   name       = "cluster-agent"
   namespace  = var.namespace
-  version    = "0.12.5"
+  version    = "0.12.6"
   values = [
     yamlencode({
       controller = {
