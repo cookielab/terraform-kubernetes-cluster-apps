901481f41060132dde5036f08a5ff1164be49904
Cookie Monster <robot@cookielab.io>
2025-05-16 03:15:33 +0000

    chore(deps): update helm release external-secrets to v0.17.0

--- modules/external-secrets/main.tf
+++ modules/external-secrets/main.tf
@@ -4,7 +4,7 @@ resource "helm_release" "external_secrets_operator" {
 
   repository = "https://charts.external-secrets.io"
   chart      = "external-secrets"
-  version    = "0.16.2"
+  version    = "0.17.0"
 
   values = [yamlencode({
     image = {
