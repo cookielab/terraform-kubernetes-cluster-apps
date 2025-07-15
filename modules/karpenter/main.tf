5125491d80a64e44aa477b44dcec234246241693
Cookie Monster <robot@cookielab.io>
2025-07-15 03:06:04 +0000

    chore(deps): update karpenter docker tag to v1.6.0

--- modules/karpenter/main.tf
+++ modules/karpenter/main.tf
@@ -93,7 +93,7 @@ resource "helm_release" "this" {
 
   repository = "oci://public.ecr.aws/karpenter"
   chart      = "karpenter"
-  version    = "1.5.2"
+  version    = "1.6.0"
 
   values = [yamlencode({
     replicas     = var.replicas
