086471b8b9181c9c39ce8789bfc875acffeb2f5b
Stepan Konecny <stepankonecny96@icloud.com>
2025-08-10 14:42:52 +0200

    conflict

--- modules/keda/main.tf
+++ modules/keda/main.tf
@@ -9,25 +9,10 @@ resource "helm_release" "keda" {
     yamlencode(merge({
       replicas = var.replicas
       logLevel = var.log_level
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1498318 (fix: consistency)
       metricsServer = merge(
         { enabled = var.metrics_server },
         length(var.resources.metrics_server) > 0 ? { resources = var.resources.metrics_server } : {}
       )
-<<<<<<< HEAD
-=======
-      metricsServer = length(var.resources.metrics_server) > 0 ? {
-        enabled   = var.metrics_server
-        resources = var.resources.metrics_server
-        } : {
-        enabled = var.metrics_server
-      }
->>>>>>> ff995d1 (feat(cluster-apps) enable resources and limits in all modules)
-=======
->>>>>>> 1498318 (fix: consistency)
       nodeSelector   = var.node_selector
       tolerations    = var.tolerations
       podAnnotations = var.pod_annotations
