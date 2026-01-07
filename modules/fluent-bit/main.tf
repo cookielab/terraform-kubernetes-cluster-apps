e2c9155abb02f97532f6387a3af580fb14ef9d5d
Cookie Monster <robot@cookielab.io>
2026-01-07 04:05:58 +0000

    chore(deps): update helm release fluent-bit to v0.54.1

--- modules/fluent-bit/main.tf
+++ modules/fluent-bit/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "fluentbit" {
   name       = "fluentbit"
   repository = "https://fluent.github.io/helm-charts"
   chart      = "fluent-bit"
-  version    = "0.54.0"
+  version    = "0.54.1"
   namespace  = var.namespace
 
   values = [yamlencode({
