619fc47a43da657d121a5dcbd4a5a07c51b08e54
Cookie Monster <robot@cookielab.io>
2026-01-23 04:06:10 +0000

    chore(deps): update karpenter-crd docker tag to v1.8.6

--- modules/karpenter/main.tf
+++ modules/karpenter/main.tf
@@ -30,7 +30,7 @@ resource "helm_release" "crds" {
 
   repository = "oci://public.ecr.aws/karpenter"
   chart      = "karpenter-crd"
-  version    = "1.6.3"
+  version    = "1.8.6"
 }
 
 data "aws_iam_policy_document" "iam_pass_role" {
