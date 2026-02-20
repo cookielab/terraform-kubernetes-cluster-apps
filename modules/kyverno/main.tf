631771bb64029f1619be0920b0adba573d1d825a
Cookie Monster <robot@cookielab.io>
2026-02-20 04:05:59 +0000

    chore(deps): update helm release kyverno to v3.7.1

--- modules/kyverno/main.tf
+++ modules/kyverno/main.tf
@@ -4,7 +4,7 @@ resource "helm_release" "this" {
 
   repository = "https://kyverno.github.io/kyverno/"
   chart      = "kyverno"
-  version    = "3.6.2"
+  version    = "3.7.1"
 
   values = [yamlencode({
     # https://github.com/kyverno/kyverno/pull/11482/checks?check_run_id=32231441646
